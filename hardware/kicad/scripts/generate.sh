255db33f783bb26650a36888f0f196c4d28d10c1
Szybet <53944559+Szybet@users.noreply.github.com>
2025-09-14 13:44:03 +0200

    rename & improve site

--- hardware/kicad/scripts/generate.sh
+++ hardware/kicad/scripts/generate.sh
@@ -5,3 +5,13 @@
 # pip install --break-system-packages --no-compile kibot
 
 ~/.local/bin/kibot -c hardware/kicad/scripts/kibot.yaml -e hardware/kicad/Yatchy/Yatchy.kicad_sch -b hardware/kicad/Yatchy/Yatchy.kicad_pcb
+
+~/.local/bin/kibot -c hardware/kicad/scripts/kibot.yaml -e hardware/kicad/modules/default/module.kicad_sch -b hardware/kicad/modules/default/module.kicad_pcb
+mv hardware/read-only/module-ibom.html hardware/read-only/module-default-ibom.html
+mv hardware/read-only/module-schematic.pdf hardware/read-only/module-default-schematic.pdf
+mv hardware/read-only/module-schematic.svg hardware/read-only/module-default-schematic.svg
+
+~/.local/bin/kibot -c hardware/kicad/scripts/kibot.yaml -e hardware/kicad/modules/BTBMHM/module.kicad_sch -b hardware/kicad/modules/BTBMHM/module.kicad_pcb
+mv hardware/read-only/module-ibom.html hardware/read-only/module-BTBMHM-ibom.html
+mv hardware/read-only/module-schematic.pdf hardware/read-only/module-BTBMHM-schematic.pdf
+mv hardware/read-only/module-schematic.svg hardware/read-only/module-BTBMHM-schematic.svg
